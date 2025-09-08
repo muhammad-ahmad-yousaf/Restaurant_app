713ac17ca129131a548ee8a592694c612d5d527e
Ahmad Yousaf <48064002+muhammad-ahmad-yousaf@users.noreply.github.com>
2025-09-08 19:06:18 +0500

    add models class

--- restaurant/cards/views.py
+++ restaurant/cards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import product
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    products = product.objects.all()     
+    return render(request,'home.html',{"products": products})
--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,6 +111,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
